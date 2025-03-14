28e129f28bae3bacbc0350f89b57ce4d158e600a
Hammad <159513856+web-arsh@users.noreply.github.com>
2025-03-14 22:18:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import { globalErrHandler } from "./middleware/middleware.js";
 const app = express();
 const port = process.env.PORT || 4444;
 const corsOption = {
-    origin: 'https://trickyhash-ox7ctu9n2-gaming-warriors-projects.vercel.app', // Allow requests from your frontend
+    origin: 'http://localhost:3000', // Allow requests from your frontend
     methods: ['GET', 'PUT'], // Allow specific HTTP methods
     // allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
 };
